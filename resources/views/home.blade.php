9316ddb257b5f1b6e706d932b95d895b9fabc060
Eduardo Cruz <eduardo@eduardocruz.com>
2020-05-09 21:47:09 -0300

    Stripe message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 
                     <div class="card-body">
                         {{-- __('Your application\'s dashboard.') --}}
-                        <p>
+                        <p>Se você já tem conta no Stripe => 
                             <a href="https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_FaRrwPcmvdiQM9JQryYgXTxetNRy2BBI&scope=read_write">
                                 <img src="/images/connect-with-stripe.png" alt="">
                             </a>
