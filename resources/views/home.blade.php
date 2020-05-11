6f0b53deabf7084ddd3e32f3df219468db7cba19
Eduardo Cruz <eduardo@eduardocruz.com>
2020-05-11 10:28:37 -0300

    Adjusting photo_url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,28 +14,23 @@
                     <div class="card-body">
                         {{-- __('Your application\'s dashboard.') --}}
                         <div class="col text-center">
-                                <a href="/videos" class="btn btn-primary mb-4">Assistir Videos Agora</a>
+                                <a href="/videos" class="btn btn-primary mb-4">Assistir Videos</a>
                         </div>
 
-
                         @foreach($users as $user)
-                            @if(isset($user->photo_url))
+                            @isset($user->email)
                                 <img
                                     src="{{$user->photo_url}}"
                                     class="img-thumbnail rounded-circle rounded-full w-8 h-8 mr-3"
+                                    width="100"
                                 />
-                            @else
-                                <img
-                                    src="https://secure.gravatar.com/avatar/{{ md5(\Illuminate\Support\Str::lower($user->email)) }}?size=64"
-                                    class="img-thumbnail rounded-circle rounded-full w-8 h-8 mr-3"
-                                />
-                            @endif
-                        @endforeach
-                        {{--
+                            {{--
                                 <span class="text-90">
                             {{ $user->name ?? $user->email ?? __('Nova User') }}
                             </span>
                             --}}
+                            @endisset
+                        @endforeach
                     </div>
                 </div>
             </div>
