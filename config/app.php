71f99dcf9a5549d8e6d37734936a034564ef40b8
Eduardo Cruz <eduardo@eduardocruz.com>
2020-05-19 22:11:37 -0300

    Adding Telescope

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
