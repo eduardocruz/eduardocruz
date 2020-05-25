650d93d83ec6aa6a0018cd7a9f17baf1c902b3b3
Eduardo Cruz <eduardo@eduardocruz.com>
2020-05-25 19:03:47 -0300

    Team invitation - improving copy and fixing typos

--- resources/views/vendor/spark/settings/teams/emails/invitation-to-existing-user.blade.php
+++ resources/views/vendor/spark/settings/teams/emails/invitation-to-existing-user.blade.php
@@ -6,15 +6,18 @@ Oi
 
 <br><br>
 
-Você já tem uma conta em https://www2.eduardocruz.com mas você convidado(a) para se juntar a um time.
+Você já tem uma conta em https://www2.eduardocruz.com mas você foi convidado(a) para se juntar a um time.
 
-Acesso o site agora, clique no ícone de sino no topo da página e nas notificações clique no botão View Invitations.
+<br><br>
 
-Ao cair na página de convite pendente, aceite o convite para participar do time de Alunos.
+Acesse o site agora, clique no ícone de sino no topo da página e nas notificações clique no botão View Invitations.
+    Ao cair na página de convite pendente, aceite o convite para participar do time de Alunos.
+
+<br><br>
 
 Lembrando que nossa aula acontece toda Segunda-feira às 20h.
 
-O link de acesso ao Zoom já se encontra disponível no site.
+    O link de acesso ao Zoom já se encontra disponível no site.
 
 <br><br>
 
