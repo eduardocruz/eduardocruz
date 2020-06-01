c9b12d249a76d515856ec7850768c897b0970030
Eduardo Cruz <eduardo@eduardocruz.com>
2020-06-01 16:59:54 -0300

    removing useremail filter from nova user

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -93,7 +93,7 @@ class User extends Resource
     public function filters(Request $request)
     {
         return [
-            new Filters\UserEmail
+            //new Filters\UserEmail
         ];
     }
 
