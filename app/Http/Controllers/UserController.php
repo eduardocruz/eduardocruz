b3274b42eb3e51b462371ee8b6586f3dd89bbc42
Eduardo Cruz <eduardo@eduardocruz.com>
2020-06-01 10:26:02 -0300

    User list order

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::orderBy('created_at', 'desc')->get();
         return view('users.index', compact('users'));
     }
 
