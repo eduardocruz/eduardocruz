481f0012ef2fcfee967a640843c1c68ad01032d3
Eduardo Cruz <eduardo@eduardocruz.com>
2020-07-27 14:44:36 -0300

    API checkin

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,30 @@
 |
 */
 
+use App\Models\Checkin;
+use Carbon\Carbon;
+
 Route::group([
     'middleware' => 'auth:api'
 ], function () {
     Route::get('me', function(){
        return auth()->user();
+    });
+    Route::get('technologies/{technology_id}', function($technology_id){
+        $technology = \App\Models\Technology::findOrFail($technology_id);
+        try {
+            $checkin = Checkin::firstOrCreate([
+                'user_id' => auth()->user()->id,
+                'technology_id' => $technology->id,
+                'checkin_at' => Carbon::now()
+            ]);
+
+            if($checkin->wasRecentlyCreated)
+                return redirect('/technologies/'.$technology->id)->with('status', 'Checkin created');
+        } catch (\Exception $exception)
+        {
+            if($exception->getCode() == 23000)
+                return redirect('/technologies/'.$technology->id)->with('status', 'You had a previous checkin today');
+        }
     });
 });
