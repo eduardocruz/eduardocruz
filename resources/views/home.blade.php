771886a28de00fb7d3a1b098bfe40d9434ceac35
Eduardo Cruz <eduardo@eduardocruz.com>
2020-08-10 17:32:46 -0300

    Zoom meeting url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,9 @@
 <home :user="user" inline-template>
     <div class="container-fluid">
         <!-- Application Dashboard -->
-        {{--
         <a href="https://us02web.zoom.us/j/87951985429" class="text-center">
-            <h3>Zoom Meeting 03/08 20h - https://us02web.zoom.us/j/87951985429</h3>
+            <h3>Zoom Meeting 10/08 20h - https://us02web.zoom.us/j/87951985429</h3>
         </a>
-        --}}
         @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
