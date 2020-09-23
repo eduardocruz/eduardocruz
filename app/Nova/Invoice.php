8afd96e30f51db7124536587b4537c646c558659
Eduardo Cruz <eduardo@eduardocruz.com>
2020-09-22 21:10:43 -0300

    Invoice on nova

--- app/Nova/Invoice.php
+++ app/Nova/Invoice.php
@@ -2,32 +2,27 @@
 
 namespace App\Nova;
 
-use App\Nova\Metrics\NewUsers;
-use App\Nova\Metrics\UsersPerDay;
-use App\Nova\Metrics\UsersPerPlan;
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\BelongsToMany;
-use Laravel\Nova\Fields\Gravatar;
-use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
-use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
+class Invoice extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\User::class;
+    public static $model = \Laravel\Spark\LocalInvoice::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -35,10 +30,8 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id', 'provider_id'
     ];
-
-    public static $orderBy = ['name' => 'asc'];
 
     /**
      * Get the fields displayed by the resource.
@@ -50,27 +43,9 @@ class User extends Resource
     {
         return [
             ID::make()->sortable(),
-
-            Gravatar::make()->maxWidth(50),
-
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', 'string', 'min:8')
-                ->updateRules('nullable', 'string', 'min:8'),
-
-            BelongsToMany::make('Technologies')->nullable(),
-
-            HasMany::make('Invoice')
+            BelongsTo::make('User')->nullable(),
+            DateTime::make('Created_At')->sortable(),
+            DateTime::make('Updated_At')->sortable(),
         ];
     }
 
@@ -82,11 +57,7 @@ class User extends Resource
      */
     public function cards(Request $request)
     {
-        return [
-            new NewUsers,
-            new UsersPerDay,
-            new UsersPerPlan
-        ];
+        return [];
     }
 
     /**
@@ -97,9 +68,7 @@ class User extends Resource
      */
     public function filters(Request $request)
     {
-        return [
-            //new Filters\UserEmail
-        ];
+        return [];
     }
 
     /**
