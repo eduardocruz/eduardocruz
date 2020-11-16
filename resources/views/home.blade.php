c2167df5e3763e6a694d7039be3293ae7a1cc0be
Eduardo Cruz <eduardo@eduardocruz.com>
2020-11-16 19:33:32 -0300

    Updating meeting date

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <!-- Application Dashboard -->
 
         <a href="https://us02web.zoom.us/j/86924363618" class="text-center">
-            <h3>Zoom Meeting 09/11 20h - https://us02web.zoom.us/j/86924363618</h3>
+            <h3>Zoom Meeting 16/11 20h - https://us02web.zoom.us/j/86924363618</h3>
         </a>
 
         @if (session('status'))
