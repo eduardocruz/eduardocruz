4a7d8b4a304de542f92eab65f65225c7610acccb
Eduardo Cruz <eduardo@eduardocruz.com>
2021-07-05 19:22:10 -0300

    Zoom link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
 <home :user="user" inline-template>
     <div class="container-fluid">
         <!-- Application Dashboard -->
-        {{--
-        <a href="#" class="text-center">
-            <h3>Nào haverá aula ao vivo nos dias 28/12/2020 e 04/01/2021</h3>
+
+        <a href="https://us02web.zoom.us/j/85992260489" class="text-center">
+            <h3>05/07/2021 - Aula1 - Projeto Piloto Automático</h3>
         </a>
-        --}}
+
         @if (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
